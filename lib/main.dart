c2906f7b8fdee317caf766a9d3188939dea75f46
Arshak_Muhammed_P_K <arshakmuhammedpk@gmail.com>
2024-09-21 16:43:27 +0000

    removed mac,ios and win

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
 
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: HomeScreen(),
     );
   }
