ecb1ff6047e1b34959b9ea4d2562207354949eca
Vandanark2004 <220687@tkmce.ac.in>
2024-09-21 22:59:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
       home: HomeScreen(),
+      login: LoginScreen(),
     );
   }
 }
